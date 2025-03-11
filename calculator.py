c36b81fc00354d43ae312cc47ae3febdb22ac3ec
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-03-11 23:18:36 +0000

    opérations de base

--- calculator.py
+++ calculator.py
@@ -1,8 +1,20 @@
-def addition(a, b) :
-    return a + b 
-def soustraction(a, b) :
-    return a - b
-def mulitiplication(a, b) :
-    return a * b
-def division(a, b) :
-    return a / b 
+from fonction import *
+
+print("\t\t\tCALCULATRICE")
+print("Opérateurs : \n 1- Addition(+) \n 2- Soustraction(-) \n 3- Multiplication(*) \n 4- Division(/) \n")
+
+a = int(input("Value1 >>> "))
+operation = int(input("Choisissez un opérateur (entre parenthèse) >>> "))
+b = int(input("Value2 >>> "))
+
+if operation == 1 :
+    print(f"Résultat = {addition(a, b)}")
+elif operation == 2 :
+    print(f"Résultat = {soustraction(a, b)}")
+elif operation == 3 :
+    print(f"Résultat = {multiplication(a, b)}")
+elif operation == 4 :
+    print(f"Résultat = {division(a, b)}")
+else :
+    print("Cet opérateur n'est pas encore prise en compte !")
+
---
+++ fonction.py
@@ -0,0 +1,9 @@
+def addition(a, b) :
+    print(a + b) 
+def soustraction(a, b) :
+    print(a - b)
+def multiplication(a, b) :
+    print(a * b)
+def division(a, b) :
+    print(a / b) 
+
